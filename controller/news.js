4c5481a1a9369f531068e5858dcf17930d707470
dxwc <dxwc@users.noreply.github.com>
2019-02-01 17:01:09 -0500

    change from require to fs.readFile and JSON parse
    require loads once and then caches, I don't want that

--- controller/news.js
+++ controller/news.js
@@ -1,10 +1,17 @@
 let router = require('express').Router();
 let render = require('./function/render.js');
+let fs     = require('fs');
+let path   = require('path');
+
+let json_file = path.join(path.dirname(__filename), '../', 'news.json');
 
 router.get('/news', async (req, res) =>
 {
     let obj = { };
-    try { obj.news = require('../news.json') } catch(err) { console.error(err) }
+
+    try { obj.news = JSON.parse(fs.readFileSync(json_file)); }
+    catch(err) { console.error(err) }
+
     return render(req, res, 'news', obj);
 });
 
